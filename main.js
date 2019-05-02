81c8ae702f5aa0f3671185033d0b1b726c0ffb49
bomber <tangyihong063@163.com>
2019-05-02 13:14:30 +0800

    对伪数组的说明

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function geiSibings(node){
   var arr = {
     length: 0
   }
-  for (i = 0; i < allchirdren.length; i++) {
+  for (i = 0; i < allchirdren.length; i++) {//该循环是为了是想一个伪数组
     if (allchirdren[i] !== node) {
       arr[arr.length] = allchirdren[i]
       arr.length += 1
