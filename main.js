ab493c4a1d40c3e4e3acb0bc52533b7774fb9059
bomber <tangyihong063@163.com>
2019-05-02 22:18:40 +0800

    另一种方法自己创建的构造函数来调用浏览器的自带的原型来实现

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-window.honghong = {}//增加命名空间honghong 
-
-Node.prototype.getSibings = function () {
-  var allchirdren = this.parentNode.children
-
-  var arr = {
-    length: 0
-  }
-  for (i = 0; i < allchirdren.length; i++) {//该循环是为了是想一个伪数组
-    if (allchirdren[i] !== this) {
-      arr[arr.length] = allchirdren[i]
-      arr.length += 1
+window.Node2 = function(node){
+  return {
+    getSibings: function () {
+      var allchirdren = node.parentNode.children
+      var arr = {
+        length: 0
+      }
+      for (i = 0; i < allchirdren.length; i++) {//该循环是为了是想一个伪数组
+        if (allchirdren[i] !== node) {
+          arr[arr.length] = allchirdren[i]
+          arr.length += 1
+        }
+      }
+      return arr
+    },
+    addClass: function (classes) {
+      classes.forEach((value) => node.classList.add(value))
     }
   }
-  return arr
 }
 
-Node.prototype.addClass = function (classes) {
-  classes.forEach((value) => this.classList.add(value))
-}
-
-
-// honghong.getSibings(item1)
-// honghong.addClass(item3, ['red', 'border'])
-
-item1.getSibings.call(item1)
-item1.addClass.call(item1,['red', 'border'])
+var node2 = Node2(item1)
+node2.getSibings()//这里因为getSibings刚好就代表了一个函数，那么可以不用写出括号，当然写出括号，括号里面不带值也是一样的效果
+node2.addClass(['red', 'border'])
