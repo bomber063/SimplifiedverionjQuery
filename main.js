1621a8d6a2c783f1ff0a8b8f5232d2a3b121ac35
bomber <tangyihong063@163.com>
2019-05-02 19:09:01 +0800

    修改原型链实现item3.getSibings()

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.honghong = {}//增加命名空间honghong 
-honghong.getSibings = function (node) {
-  var allchirdren = item1.parentNode.children
+honghong.getSibings = function () {
+  var allchirdren =this.parentNode.children
 
   var arr = {
     length: 0
   }
   for (i = 0; i < allchirdren.length; i++) {//该循环是为了是想一个伪数组
-    if (allchirdren[i] !== node) {
+    if (allchirdren[i] !== this) {
       arr[arr.length] = allchirdren[i]
       arr.length += 1
     }
@@ -14,9 +14,16 @@ honghong.getSibings = function (node) {
   return arr
 }
 
-honghong.addClass = function (node, classes) {
-  classes.forEach((value) => node.classList.add(value))
+honghong.addClass = function (classes) {
+  classes.forEach((value) => this.classList.add(value))
 }
 
-honghong.getSibings(item1)
-honghong.addClass(item3, ['red', 'border'])
+Node.prototype.getSibings=honghong.getSibings
+Node.prototype.addClass=honghong.addClass
+
+
+// honghong.getSibings(item1)
+// honghong.addClass(item3, ['red', 'border'])
+
+item1.getSibings()
+item1.addClass(['red', 'border'])
